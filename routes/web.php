6552d0097641d79ef95b3264551238ebc200208d
dcarvallo <danielcarvallo.molina@gmail.com>
2020-05-04 18:54:13 -0400

    integrado admin lte plantilla

--- routes/web.php
+++ routes/web.php
@@ -22,5 +22,5 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('admin', function(){
-    return view('admin.index');
+    return view('admin.plantilla');
 });
